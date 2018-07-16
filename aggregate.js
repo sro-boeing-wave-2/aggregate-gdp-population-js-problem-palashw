60c4e6f18c96b27acaa5edd1fc3889f37735b5bd
Palash <palashw173@gmail.com>
2018-07-16 18:32:56 +0530

    aggregate optimised code made after changes and review

--- aggregate.js
+++ aggregate.js
@@ -4,82 +4,57 @@
  */
 const fs = require('fs');
 
-const fileRead = filepath1 => new Promise((resolve, reject) => {
-  fs.readFile(filepath1, 'utf8', (err, result) => {
+const fileRead = filepath => new Promise((resolve, reject) => {
+  fs.readFile(filepath, 'utf8', (err, result) => {
     if (err) reject(err);
     else resolve(result);
   });
 });
 
-const aggregate = filePath => new Promise((resolve1, reject1) => {
+const writeFile = (filepath, data) => new Promise((resolve, reject) => {
+  fs.writeFile(filepath, data, (err) => {
+    if (err) reject(err);
+    else resolve(data);
+  });
+});
+
+const aggregate = filePath => new Promise((resolve, reject) => {
   Promise.all([fileRead(filePath), fileRead('countriesmap.txt')]).then((values) => {
     // converting country continent to map
-    const mapContents = values[1];
-    const splitMapstring = mapContents.split('\n');
-    let splitMapByComma;
-    const countryContinentMap = new Map();
-    for (let i = 0; i < splitMapstring.length; i += 1) {
-      splitMapByComma = splitMapstring[i].split(',');
-      splitMapByComma[1] = splitMapByComma[1].replace(/\r/g, '');
-      countryContinentMap.set(splitMapByComma[0], splitMapByComma[1]);
-    }
 
-    // cleaning datafile.csv
-    let countryObjects;
-    const countryMap = [];
-    let data = values[0];
-    data = data.replace(/['"]+/g, '');
-    const splitData = data.toString().split('\n');
+    const countryContinentMap = values[1].split('\n').reduce((accumulator, row) => {
+      const [country, continent] = row.split(',');
+      accumulator[country] = continent;
+      return accumulator;
+    }, {});
+
+    const data = values[0].replace(/['"]+/g, '');
+    const splitData = data.split('\n');
     const headers = splitData[0].split(',');
-    for (let i = 1; i < splitData.length; i += 1) {
-      const cleandata = splitData[i].split(',');
-      countryObjects = {};
-      for (let j = 0; j < cleandata.length; j += 1) {
-        countryObjects[headers[j]] = cleandata[j];
-      }
-      countryMap.push(countryObjects);
-    }
-    // mapping continent to countrydata
-    const continentlist = [];
-    for (let i = 0; i < countryMap.length; i += 1) {
-      if (countryMap[i]['Country Name'] !== 'European Union') {
-        countryMap[i].continent = countryContinentMap.get(countryMap[i]['Country Name']);
-        continentlist.push(countryContinentMap.get(countryMap[i]['Country Name']));
-      }
-    }
-    const continentsplitData = [...new Set(continentlist)];
-    continentsplitData.splice(6, 1);
-    const finalsplitData = [];
-    const countryObjectsectdefined = {};
-    // aggregating
-    for (let i = 0; i < continentsplitData.length; i += 1) {
-      let sumpop = 0;
-      let sumgdp = 0;
-      for (let j = 0; j < countryMap.length; j += 1) {
-        if (continentsplitData[i] === countryMap[j].continent) {
-          sumpop += parseFloat(countryMap[j]['Population (Millions) - 2012']);
-          sumgdp += parseFloat(countryMap[j]['GDP Billions (US Dollar) - 2012']);
+    const indexOfPopulation = headers.indexOf('Population (Millions) - 2012');
+    const indexOfCountry = headers.indexOf('Country Name');
+    const indexOfGDP = headers.indexOf('GDP Billions (US Dollar) - 2012');
+    const aggregatedValues = {};
+
+    splitData.forEach((rowTxt) => {
+      const row = rowTxt.split(',');
+      if (countryContinentMap[row[indexOfCountry]] !== undefined && row[indexOfCountry] !== 'European Union') {
+        if (aggregatedValues[countryContinentMap[row[indexOfCountry]]] === undefined) {
+          aggregatedValues[countryContinentMap[row[indexOfCountry]]] = {};
+          aggregatedValues[countryContinentMap[row[indexOfCountry]]]
+            .GDP_2012 = parseFloat(row[indexOfGDP]);
+          aggregatedValues[countryContinentMap[row[indexOfCountry]]]
+            .POPULATION_2012 = parseFloat(row[indexOfPopulation]);
+        } else {
+          aggregatedValues[countryContinentMap[row[indexOfCountry]]]
+            .GDP_2012 += parseFloat(row[indexOfGDP]);
+          aggregatedValues[countryContinentMap[row[indexOfCountry]]]
+            .POPULATION_2012 += parseFloat(row[indexOfPopulation]);
         }
       }
-      const name = {};
-      name.GDP_2012 = sumgdp;
-      name.POPULATION_2012 = sumpop;
-      finalsplitData.push(name);
-    }
-    // making final object in required format
-    for (let i = 0; i < continentsplitData.length; i += 1) {
-      countryObjectsectdefined[continentsplitData[i]] = finalsplitData[i];
-    }
-    const filewrite = (filepath3, finaldata) => new Promise((resolve, reject) => {
-      fs.writeFile(filepath3, finaldata, (err) => {
-        if (err) reject(err);
-        resolve(finaldata);
-      });
     });
-    filewrite('./output/output.json', JSON.stringify(countryObjectsectdefined)).then(() => resolve1(), err => reject1(err));
-  })
-    .catch(errors => console.log(errors));
+    writeFile('./output/output.json', JSON.stringify(aggregatedValues)).then(() => resolve(), err => reject(err));
+  }).catch(errors => console.log(errors));
 });
 
 module.exports = aggregate;
-// aggregate('./data/datafile.csv');
---
+++ aggregateold.js
@@ -0,0 +1,85 @@
+/**
+ * Aggregates GDP and Population Data by Continents
+ * @param {*} filePath
+ */
+const fs = require('fs');
+
+const fileRead = filepath1 => new Promise((resolve, reject) => {
+  fs.readFile(filepath1, 'utf8', (err, result) => {
+    if (err) reject(err);
+    else resolve(result);
+  });
+});
+
+const aggregate = filePath => new Promise((resolve1, reject1) => {
+  Promise.all([fileRead(filePath), fileRead('countriesmap.txt')]).then((values) => {
+    // converting country continent to map
+    const mapContents = values[1];
+    const splitMapstring = mapContents.split('\n');
+    let splitMapByComma;
+    const countryContinentMap = new Map();
+    for (let i = 0; i < splitMapstring.length; i += 1) {
+      splitMapByComma = splitMapstring[i].split(',');
+      splitMapByComma[1] = splitMapByComma[1].replace(/\r/g, '');
+      countryContinentMap.set(splitMapByComma[0], splitMapByComma[1]);
+    }
+
+    // cleaning datafile.csv
+    let countryObjects;
+    const countryMap = [];
+    let data = values[0];
+    data = data.replace(/['"]+/g, '');
+    const splitData = data.toString().split('\n');
+    const headers = splitData[0].split(',');
+    for (let i = 1; i < splitData.length; i += 1) {
+      const cleandata = splitData[i].split(',');
+      countryObjects = {};
+      for (let j = 0; j < cleandata.length; j += 1) {
+        countryObjects[headers[j]] = cleandata[j];
+      }
+      countryMap.push(countryObjects);
+    }
+    // mapping continent to countrydata
+    const continentlist = [];
+    for (let i = 0; i < countryMap.length; i += 1) {
+      if (countryMap[i]['Country Name'] !== 'European Union') {
+        countryMap[i].continent = countryContinentMap.get(countryMap[i]['Country Name']);
+        continentlist.push(countryContinentMap.get(countryMap[i]['Country Name']));
+      }
+    }
+    const continentsplitData = [...new Set(continentlist)];
+    continentsplitData.splice(6, 1);
+    const finalsplitData = [];
+    const countryObjectsectdefined = {};
+    // aggregating
+    for (let i = 0; i < continentsplitData.length; i += 1) {
+      let sumpop = 0;
+      let sumgdp = 0;
+      for (let j = 0; j < countryMap.length; j += 1) {
+        if (continentsplitData[i] === countryMap[j].continent) {
+          sumpop += parseFloat(countryMap[j]['Population (Millions) - 2012']);
+          sumgdp += parseFloat(countryMap[j]['GDP Billions (US Dollar) - 2012']);
+        }
+      }
+      const name = {};
+      name.GDP_2012 = sumgdp;
+      name.POPULATION_2012 = sumpop;
+      finalsplitData.push(name);
+    }
+    // making final object in required format
+    for (let i = 0; i < continentsplitData.length; i += 1) {
+      countryObjectsectdefined[continentsplitData[i]] = finalsplitData[i];
+    }
+    const filewrite = (filepath3, finaldata) => new Promise((resolve, reject) => {
+      fs.writeFile(filepath3, finaldata, (err) => {
+        if (err) reject(err);
+        resolve(finaldata);
+      });
+    });
+    filewrite('./output/output.json', JSON.stringify(countryObjectsectdefined)).then(() => resolve1(), err => reject1(err));
+  })
+    .catch(errors => console.log(errors));
+});
+
+module.exports = aggregate;
+// aggregate('./data/datafile.csv');
